25f09496b1263fc9f7a68c367154188ac2275728
Christine Casebolt <christine.casebolt@gmail.com>
2016-10-23 21:28:05 -0700

    comments and delete method as well as auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+    def index
+      @articles = Article.all
+    end
+
   def index
   @articles = Article.all
   end
@@ -33,14 +39,14 @@ end
       render 'edit'
     end
   end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
 end
 
-def destroy
-  @article = Article.find(params[:id])
-  @article.destroy
-
-  redirect_to articles_path
-end
 
 private
   def article_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  resources :articles # makes articls the CRUD item (our app will have the ability to Create Read Update and Delete instances of these)
-  root 'welcome#index' #makes this the root or home page
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ # makes articls the CRUD item (our app will have the ability to Create Read Update and Delete instances of these)
+  resources :articles do
+  resources :comments
+ #makes this the root or home page
+end
+    root 'welcome#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
